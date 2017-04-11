ed291802a0b747979080ba2e00149004d4aed275
Michael Jordan <mikesjordan@gmail.com>
2017-04-11 16:17:28 -0600

    this is a page we made covering basic html tags and deployments. I test aligning right and center and it worked! we also practiced commit and push HERE GOES NOTHING

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<meta name="viewport"
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<title>This is a BORING Html Sandbox</title>
+	</head>
+	<body>
+		<H1 align="right">SANDBOX ON THE RIGHT</H1>
+		<p align="right">I'm SandBoX check me out <BR>
+		praise KEK </p>
+		<p align="center"> BASED SAND BOX CENTERED AND ALL</p>
+		<p>I'm a Berned out Sanders Box</p>
+</html>
