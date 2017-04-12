a3261f904b14cd805ba7042b62b2af3870507b70
Michael Jordan <mikesjordan@gmail.com>
2017-04-12 09:38:22 -0600

    added a table about dogs and cats because i dont know anything about walruses

--- index.php
+++ index.php
@@ -12,12 +12,51 @@
 	</head>
 	<body>
 		<header>
-			<H1>WALRUS</H1>
+			<H1>WALRUUUUUUSIA</H1>
 		</header>
 		<main>
-		<h2>WAAAAAAAAAAAAAAALRUUUUUUUUUUUS</h2>
+		<h2>Facts about Walruses</h2>
 			<p>The walrus (Odobenus rosmarus) is a large Arctic marine mammal with flippers, a broad head, short muzzle, small eyes, tusks and whiskers. Scientists recognize two subspecies of walrus – the Atlantic walrus and the Pacific walrus.</p>
 		<p>Walruses are cinnamon brown in color. They are able to turn their hind flippers forward to aid in movement on land. Their front flippers are large and each has five digits. Males have special air sacs that are used to make a bell-like sound.</p>
 		<p>Both males and females have large tusks that are used for defense, cutting through ice and getting out of the water. The tusks can be more than three feet long in males and about two and a half feet long in females.</p>
-			<img src="images/walrus.jpg" alt="walrus on sub"
+			<h3>Super Hero Dan on a Russian sub hanging with a Walrus</h3>
+			<img src="images/walrus.jpg" alt="walrus on sub">
+			<p>Make sure to check out Daniel on his Twitter and tell him you love walruses too: <a href="https://twitter.com/SuperHeronDan" target="_blank">@SuperHeronDan</a></p>
+			<h2>Here is a list about Walruses</h2>
+			<ul>
+				<li>The scientific name for a walrus is Odobenus Rosmarus</li>
+				<li>Walrus is a marine mammal</li>
+				<li>A male walrus is called a bull</li>
+				<li>A female walrus is called a cow</li>
+				<li>A baby walrus is called a calf</li>
+				<li>Walruses spend half their time on land and the other half in water.</li>
+				<li>There are two sub-species of walruses.</li>
+			</ul>
+			<h2>Different types of Walruses</h2>
+			<ol>
+				<li> the Pacific walrus (Odobenus rosmarus divergens)</li>
+				<li>the Atlantic walrus (Odobenus rosmarus rosmarus)</li>
+				<li>the Laptev walrus (Odobensus rosmarus laptevi)</li>
+			</ol>
+			<h2>Dogs &amp: Cats</h2>
+			<table>
+				<tr>
+					<th>Dogs</th>
+					<th>Cats</th>
+				</tr>
+				<tr>
+					<td>Bull mastiff</td>
+				<td>Tabby</td>
+				</tr>
+				<tr>
+					<td>Beagle</td>
+				<td>Persian</td>
+				</tr>
+				<tr>
+					<td>Boston Terrier</td>
+					<td>Scottish Fold</td>
+				</tr>
+			</table>
+			<p>Check out are site admin <a href="about/index.php">SUPER AWESOME SITE ADMIN</a></p>
+
 </html>
