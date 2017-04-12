c7c412cbb71081dc8a64562c496701f0f67142b8
Michael Jordan <mikesjordan@gmail.com>
2017-04-12 08:23:06 -0600

    I've added a css dir and for adding style sheet within my php file

--- index.php
+++ index.php
@@ -2,15 +2,17 @@
 <html>
 	<head>
 		<meta charset="UTF-8"/>
-		<meta name="viewport"
-				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
+		<meta name="viewport"/>
 		<meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+		<!--this is a comment about me adding a style.css-->
+		<link rel="stylesheet" href="css/style.css" type="text/css"/>
+				content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"/>
 		<title>This is a BORING Html Sandbox</title>
 	</head>
 	<body>
 		<H1 align="right">SANDBOX ON THE RIGHT</H1>
 		<p align="right">I'm SandBoX check me out <BR>
-		praise KEK </p>
-		<p align="center"> BASED SAND BOX CENTERED AND ALL</p>
-		<p>I'm a Berned out Sanders Box</p>
+		I'm on the right </p>
+		<p align="center"> centered sandbox</p>
+		<p>to the left sand box to the windows to the wall</p>
 </html>
