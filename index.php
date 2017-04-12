1db430333539cfb2b3fe95135f47e7f987caf780
Michael Jordan <mikesjordan@gmail.com>
2017-04-12 08:55:04 -0600

    cleaned up the align tags as its bad practice and added more depth and gave the page a topic / some new text on the topic also added img

--- index.php
+++ index.php
@@ -8,12 +8,16 @@
 		<link rel="stylesheet" href="css/style.css" type="text/css"/>
 		<!--this is a comment about me adding a java script-->
 		<script type="text/javascript" src="/java/js.js"></script>
-		<title>This is a BORING Html Sandbox</title>
+		<title>This is a page about Walruses</title>
 	</head>
 	<body>
-		<H1 align="right">SANDBOX ON THE RIGHT</H1>
-		<p align="right">I'm SandBoX check me out <BR>
-		I'm on the right </p>
-		<p align="center"> centered sandbox</p>
-		<p>to the left sand box to the windows to the wall</p>
+		<header>
+			<H1>WALRUS</H1>
+		</header>
+		<main>
+		<h2>WAAAAAAAAAAAAAAALRUUUUUUUUUUUS</h2>
+			<p>The walrus (Odobenus rosmarus) is a large Arctic marine mammal with flippers, a broad head, short muzzle, small eyes, tusks and whiskers. Scientists recognize two subspecies of walrus – the Atlantic walrus and the Pacific walrus.</p>
+		<p>Walruses are cinnamon brown in color. They are able to turn their hind flippers forward to aid in movement on land. Their front flippers are large and each has five digits. Males have special air sacs that are used to make a bell-like sound.</p>
+		<p>Both males and females have large tusks that are used for defense, cutting through ice and getting out of the water. The tusks can be more than three feet long in males and about two and a half feet long in females.</p>
+			<img src="images/walrus.jpg" alt="walrus on sub"
 </html>
